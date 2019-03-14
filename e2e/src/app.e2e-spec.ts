73fe351a543a8da47b2d36461bc243dc3382d6de
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-03-14 16:22:06 +0900

    test: fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to page!');
+    expect(page.getTitleText()).toEqual('Welcome to scaffold!');
   });
 
   afterEach(async () => {
